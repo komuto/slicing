c2b727058f7bafcdb6bc17e5c789a060f5c8236c
devid_skyshi <devid.septian@reebonz.com>
2017-06-07 07:29:53 +0700

    on progress

---
+++ README.md
@@ -0,0 +1,11 @@
+Pastikan gulp telah diinstall secara global. Jika belum, jalankan perintah berikut.
+
+sudo npm install -g gulp
+
+Setelah selesai clone repository ini, jalankan perintah berikut pada direktori projek ini.
+
+sudo npm install
+
+Untuk menjalankan sass, gunakan perintah berikut.
+
+gulp style atau gulp watch atau gulp
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+var gulp = require('gulp'),
+  plumber = require('gulp-plumber'),
+  autoprefixer = require('gulp-autoprefixer'),
+  connect = require('gulp-connect');
+var sass = require('gulp-sass');
+ 
+gulp.task('connect', function() {
+   connect.server({
+        livereload: true
+    });
+});
+gulp.task('styles', function(){
+  return  gulp.src('sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(plumber())
+    .pipe(gulp.dest('./css'))
+    .pipe(autoprefixer())
+    .pipe(connect.reload());
+    //.pipe(livereload());
+});
+gulp.task('scripts', function(){
+  gulp.src('js/*.js')
+    .pipe(plumber())
+    // .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(connect.reload());
+    //.pipe(livereload());
+});
+gulp.task('html', function () {
+  gulp.src('./templates/*.html')
+    .pipe(plumber())
+    .pipe(connect.reload());
+    //.pipe(livereload());
+});
+gulp.task('image', function(){
+  // gulp.src('sass/*.scss')
+  // .pipe(sass())
+   gulp.src('./images/*')
+    .pipe(plumber())
+    // .pipe(sass({
+    //   style: 'compressed'
+    // }))
+    .pipe(connect.reload());
+    //.pipe(livereload());
+});
+
+gulp.task('watch', function () {
+  //livereload.listen();
+  gulp.watch(['./templates/**/*.html'], ['html']);
+  gulp.watch(['./sass/**/*.sass'], ['styles']);
+ gulp.watch(['js/*.js'], ['scripts']);
+  gulp.watch(['./images/*'], ['image']);
+});
+
+gulp.task('default', ['image', 'styles', 'connect', 'watch']);
